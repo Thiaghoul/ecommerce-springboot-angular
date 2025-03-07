42af0c1fee59b92862b47b9ccf1f784f585b0857
thiaghoul <thiagohnunes.w@gmail.com>
2025-03-06 23:41:12 -0300

    listProducts method and product-list.component.html loop created

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+
+  products : Product[] = [];
+  constructor(private productService : ProductService){}
+
+  ngOnInit(): void{
+    this.listProducts();
+
+  }
+
+  listProducts(){
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
 
 }
