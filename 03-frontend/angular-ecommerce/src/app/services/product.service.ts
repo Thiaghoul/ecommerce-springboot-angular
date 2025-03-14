d1a6750d9dd2b1279ee526297662a05834d05024
thiaghoul <thiagohnunes.w@gmail.com>
2025-03-14 00:08:38 -0300

    updated the handleSearchProducts method and added the searchProductPaginate added. keyword search pagination implemented

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -9,45 +9,55 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
-  
-  
+
+
 
   private baseUrl = "http://localhost:8080/api/products";
 
   private categoryUrl = "http://localhost:8080/api/product-category";
 
-  constructor(private httpClient : HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  getProduct(thePoductId: number) : Observable<Product> {
-    
+  getProduct(thePoductId: number): Observable<Product> {
+
     const productUrl = `${this.baseUrl}/${thePoductId}`;
-    
+
     return this.httpClient.get<Product>(productUrl);
   }
 
-  getProductListPaginate(thePage : number,
-                          thePageSize : number,
-                          theCategoryId : number) : Observable<GetResponseProducts>{
-  
-      const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
-                      + `&page=${thePage}&size=${thePageSize}`;
-  
-      return this.httpClient.get<GetResponseProducts>(searchUrl);
-    }
+  getProductListPaginate(thePage: number,
+    thePageSize: number,
+    theCategoryId: number): Observable<GetResponseProducts> {
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
 
 
-  getProductList(theCategoryId: number) : Observable<Product[]>{
+  getProductList(theCategoryId: number): Observable<Product[]> {
 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
     return this.getProducts(searchUrl);
   }
 
-  searchProducts(theKeyword: string) : Observable<Product[]> {
-    
+  searchProducts(theKeyword: string): Observable<Product[]> {
+
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
 
     return this.getProducts(searchUrl);
+  }
+
+  searchProductPaginate(thePage: number,
+                            thePageSize: number,
+                            theKeyword: string): Observable<GetResponseProducts> {
+
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
   private getProducts(searchUrl: string): Observable<Product[]> {
@@ -56,7 +66,7 @@ export class ProductService {
     );
   }
 
-  getProductCategories() : Observable<ProductCategory[]>{
+  getProductCategories(): Observable<ProductCategory[]> {
 
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
