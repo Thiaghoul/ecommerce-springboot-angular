0de4476922eb7e6d6c577c051a7c719830f5cf26
thiaghoul <thiagohnunes.w@gmail.com>
2025-03-16 13:57:29 -0300

    Country and State repository created with findByCountryCode query method added

--- 02-backend/spring-boot-ecommerce/src/main/java/com/thiaghoul/ecommerce/entities/Country.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/thiaghoul/ecommerce/entities/Country.java
@@ -1,5 +1,6 @@
 package com.thiaghoul.ecommerce.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,5 +26,6 @@ public class Country{
     private String name;
 
     @OneToMany(mappedBy = "country")
+    @JsonIgnore
     private List<State> states;
 }
