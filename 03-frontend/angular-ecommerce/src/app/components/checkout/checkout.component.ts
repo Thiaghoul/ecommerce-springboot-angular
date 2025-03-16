aa57370897c2b3e64393d41b40b87e8c56f349b8
thiaghoul <thiagohnunes.w@gmail.com>
2025-03-16 13:21:13 -0300

    credit card months field dependency to credit card years field added

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -92,4 +92,32 @@ export class CheckoutComponent implements OnInit {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer')?.value);
   }
+
+  handleMonthsAndYears() {
+    
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+    
+    let startMonth: number;
+
+    if (currentYear === selectedYear){
+      startMonth = new Date().getMonth() + 1;
+    
+    }else{
+      startMonth = 1;
+
+    }
+
+    this.payMoreGetLessFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      } 
+    )
+  }
 }
