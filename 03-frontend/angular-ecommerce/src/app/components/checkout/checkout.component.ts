5aa3c5a2e442a2779c4db8c9af84c527573c45db
thiaghoul <thiagohnunes.w@gmail.com>
2025-03-16 20:33:56 -0300

    Shipping address form validation added

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -35,15 +35,14 @@ export class CheckoutComponent implements OnInit {
       customer: this.formBuilder.group({
         firstName: new FormControl('', [Validators.required, Validators.minLength(2), PayMoreGetLessValidators.notOnlyWhitespace]),
         lastName: new FormControl('', [Validators.required, Validators.minLength(2), PayMoreGetLessValidators.notOnlyWhitespace]),
-        email: new FormControl('',
-                               [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
+        email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
-        country: [''],
-        street: [''],
-        city: [''],
-        state: [''],
-        zipCode: ['']
+        country: new FormControl('', [Validators.required]),
+        street: new FormControl('', [Validators.required, Validators.minLength(2), PayMoreGetLessValidators.notOnlyWhitespace]),
+        city: new FormControl('', [Validators.required, Validators.minLength(2), PayMoreGetLessValidators.notOnlyWhitespace]),
+        state: new FormControl('', [Validators.required]),
+        zipCode: new FormControl('', [Validators.required, Validators.minLength(2), PayMoreGetLessValidators.notOnlyWhitespace])
       }),
       billingAddress: this.formBuilder.group({
         country: [''],
@@ -98,6 +97,19 @@ export class CheckoutComponent implements OnInit {
   get firstName(){return this.checkoutFormGroup.get('customer.firstName');}
   get lastName(){return this.checkoutFormGroup.get('customer.lastName')}
   get email(){return this.checkoutFormGroup.get('customer.email')}
+
+  get shippingAddressStreet(){return this.checkoutFormGroup.get('shippingAddress.street')}
+  get shippingAddressCity(){return this.checkoutFormGroup.get('shippingAddress.city')}
+  get shippingAddressState(){return this.checkoutFormGroup.get('shippingAddress.state')}
+  get shippingAddressZipCode(){return this.checkoutFormGroup.get('shippingAddress.zipCode')}
+  get shippingAddressCountry(){return this.checkoutFormGroup.get('shippingAddress.country')}
+
+  get billingAddressStreet(){return this.checkoutFormGroup.get('billingAdress.street')}
+  get billingAddressCity(){return this.checkoutFormGroup.get('billingAdress.city')}
+  get billingAddressState(){return this.checkoutFormGroup.get('billingAdress.state')}
+  get billingAddressZipCode(){return this.checkoutFormGroup.get('billingAdress.zipCode')}
+  get billingAddressCountry(){return this.checkoutFormGroup.get('billingAdress.country')}
+
 
   copyShippingAddressToBillingAddress(event: Event) {
     const checkbox = event.target as HTMLInputElement;
