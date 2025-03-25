fa72f52498a2120463aac60bff067bc278ffba36
thiaghoul <thiagohnunes.w@gmail.com>
2025-03-24 21:38:43 -0300

    otka dependencies installed and added develop app config(used to integrate the apllication with otka)

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc:{
+        clientid: '0oanzjnw5usr2P2O95d7',
+        issuer: 'https://dev-37606743.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
